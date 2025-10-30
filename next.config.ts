e0a4e7899df9659ff3d92477ab9b2b8c66384e38
Testimony Chagona <testimonychagona01@gmail.com>
2025-10-31 00:43:47 +0200

    course card created

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "testalyte-lms.t3.storage.dev",
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
